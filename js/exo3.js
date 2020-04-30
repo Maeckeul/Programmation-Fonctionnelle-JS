fc3f54c8f85fca6d296d8f16bffcd50cd2c26986
Alexis Vincent <44369725+alexisOclock@users.noreply.github.com>
2020-04-30 14:57:42 +0200

    Initial commit

---
+++ js/exo3.js
@@ -0,0 +1,19 @@
+/*
+
+Chacun son tour
+===============
+
+Un intégrateur nous a gentiment préparé des div en `display: none`. 
+C'est à nous de les afficher en JS.
+
+Objectifs
+---------
+- Cibler les div avec la classe CSS `.box`
+- Afficher les div une à une avec 1 seconde entre chaque
+
+Notions
+-------
+- DOM : document.querySelectorAll ou getElementsByClassName
+- JS : for ou forEach / setTimeout
+
+*/
