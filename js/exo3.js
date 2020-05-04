bf69f2c9f0a9156f664c5fd6c8ec52b51273bbfa
alexisOclock <alexis@oclock.io>
2020-05-04 15:16:29 +0200

    exo 3 avec forEach, vive les fonctions

--- js/exo3.js
+++ js/exo3.js
@@ -17,3 +17,16 @@ Notions
 - JS : for ou forEach / setTimeout
 
 */
+
+// je cible les div.box
+const items = document.querySelectorAll('.box');
+
+// je parcours mes items avec forEach (querySelectorAll retourne un NodeList possédant la méthode forEach)
+
+items.forEach((item, index) => {
+    setTimeout(() => {
+        // item représente la valeur courante, ici un element du DOM
+        // on peut modifier ses styles via la propriété style
+        item.style.display = 'block';
+    }, 1000 * index);
+});
