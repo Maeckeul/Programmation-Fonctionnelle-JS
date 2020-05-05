f97248cd56d114c19591edb567e839cba0c4f306
alexisOclock <alexis@oclock.io>
2020-05-05 09:41:15 +0200

    exo 5, find

--- js/exo5.js
+++ js/exo5.js
@@ -42,18 +42,55 @@ var data = [
   },
 ];
 
+// définition de la fonction qui retourne un user
 function findUser(login, users) {
-  
+  const foundUser = users.find(function(currentUser) {
+    // if (currentUser.login === login) {
+    //   return true;
+    // }
+    // else {
+    //   return false;
+    // }
+    return currentUser.login === login;
+  });
+  return foundUser;
 }
 
+// tests d'execution
+const testFindUser = findUser('navilsy', data);
+const secondTest = findUser('totoDu02', [
+  {
+    name: 'truc',
+    login: 'bidule',
+  },
+  {
+    name: 'John',
+    login: 'totoDu02',
+  }
+]);
+
+console.log(testFindUser);
+console.log(secondTest);
+
+// find est une méthode des array qui prend une fonction de rappel ou callback en paramètre
+// cette fonction de rappel est executée par find sur chaque valeur du tableau et prend en paramètre la valeur courante
+// si la fonction de rappel renvoie true, alors find arrête de parcourir le tableau et renvoie la valeur courante, sinon il va tester la valeur suivante
 
 
 
+// // version 1 fléchée 
+// const findUser = (login, users) => {
+//   const foundUser = users.find((currentUser) => {
+//     return currentUser.login === login;
+//   });
+//   return foundUser;
+// };
 
-
-
-
-
+// // version 2 fléchée
+// const findUser = (login, users) => {
+//   const foundUser = users.find((currentUser) => currentUser.login === login);
+//   return foundUser;
+// };
 
 
 
